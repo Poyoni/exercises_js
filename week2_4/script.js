0925b17207a3fbfced9d23d64d9b901f8df3e336
poyoni <yonapollak1@gmail.com>
2024-09-11 11:57:06 +0300

    finish

--- week2_4/script.js
+++ week2_4/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     const selectCars = document.querySelector('select');
     const startButton = document.querySelector('button');
@@ -22,27 +23,35 @@ function Cars(i){
     img.src =`images/car${i}.png`;
     img.id = i;
     newDiv.appendChild(img);
+    const startTime = Date.now();
 
-    Drive(i);
+    Drive(i,startTime);
 }
 
-function Drive(i) {
+function Drive(i,startTime) {
     let position =20;
     const TimeDrive = setInterval(() => {
-    const image = document.getElementById(i);
-    position += getRandomInt(1,50);
-    image.style.left = `${position}px`;
+        const image = document.getElementById(i);
+        position += getRandomInt(1, 50);
+        image.style.left = `${position}px`;
 
-    if(position >= 1350 )
-        clearInterval(TimeDrive)
-    }, 200)
+        if (position >= 1350) {
+            clearInterval(TimeDrive);
+            const travelTime = Date.now() - startTime;
+            const h1 = document.createElement('h1');
+            h1.innerText = `Car ${i} has finished the race in ${travelTime / 1000} seconds!`;
+            document.body.appendChild(h1);
+        } else {
+        }
+    }, 200);
 }
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-
-
-
+function raceReset(){
+    const ResetButton = document.createElement('button');
+    ResetButton.id = "ResetButton";
+    document.body.appendChild(ResetButton)
+}
